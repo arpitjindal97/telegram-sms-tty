ab1960f101334c1b2df33050fef50838f778ee1f
Arpit Agarwal <arpitjindal1997@hotmail.com>
2017-08-15 00:15:28 +0530

    upload! instead of scp

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,9 +3,9 @@ set :host , "pi@192.168.0.8"
 task :deploy do
 	run_locally do
 		execute 'mvn clean package'
-		execute "scp deployments/telegram_sms_tty-1.0.jar #{fetch(:host)}:/home/pi/Desktop/"
 	end
 	on fetch(:host) do
+		upload! "deployments/telegram_sms_tty-1.0.jar","/home/pi/Desktop/"
 		execute "kill -9 $(jps | awk '/telegram/ { print $1 }')"
 	end
 	run_locally do
